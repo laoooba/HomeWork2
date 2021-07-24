dc90edb0568c40f01adfc77894932a5a4c9e2174
laoooba <yama10taku31@gmail.com>
2021-07-24 09:39:34 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
+  end
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
